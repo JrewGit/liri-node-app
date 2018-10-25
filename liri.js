7cf5dbaf25e53b13a5225bab1ae6cde68a6083a7
Jrew <jrewmohamed@gmail.com>
2018-10-25 14:53:14 -0400

    Fixed bug that didnt allow do-what-it-says to run concert-this

--- liri.js
+++ liri.js
@@ -17,17 +17,14 @@ if (process.argv[2] === "concert-this") {
     let bandsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     // console.log(bandsURL)
     request(bandsURL, function (error, response, body) {
+        console.log("something");
         if (!error && response.statusCode === 200) {
             for (let i = 0; i < JSON.parse(body).length; i++) {
-                // if (JSON.parse(body).length > 0) {
-                // console.log(JSON.parse(body)[i].venue);
+                console.log(response.statusCode)
                 console.log(JSON.parse(body)[i].venue.name);
                 console.log(`${JSON.parse(body)[i].venue.city}, ${JSON.parse(body)[i].venue.country}`);
                 console.log(moment(JSON.parse(body)[i].datetime).format('L'));
                 console.log("----------");
-                // } else {
-                //     console.log("This artist doesn't have a tour scheduled.")
-                // }
             }
         }
     })
@@ -100,6 +97,64 @@ if (process.argv[2] === "concert-this") {
 
 } else if (process.argv[2] === "do-what-it-says") {
 
+    fs.readFile("./random.txt", "utf8", function (err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            // console.log(data);
+            let dataList = data.split(",");
+            // console.log(dataList);
+            if (dataList[0] === "concert-this") {
+                let artist = dataList[1];
+                let quoteless = artist.replace(/"/g,"")
+                let bandsURL = "https://rest.bandsintown.com/artists/" + quoteless + "/events?app_id=codingbootcamp";
+                // console.log(bandsURL);
+                request(bandsURL, function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        for (let i = 0; i < JSON.parse(body).length; i++) {
+                            console.log(JSON.parse(body)[i].venue.name);
+                            console.log(`${JSON.parse(body)[i].venue.city}, ${JSON.parse(body)[i].venue.country}`);
+                            console.log(moment(JSON.parse(body)[i].datetime).format('L'));
+                            console.log("----------");
+                        }
+                    }
+                })
 
+            } else if (dataList[0] === "spotify-this-song") {
 
-}
+                spotify.search({ type: 'track', query: dataList[1], limit: 1 }, function (err, data) {
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    }
+                    // console.log(data.tracks.items);
+                    console.log("----------");
+                    console.log(data.tracks.items[0].album.artists[0].name);
+                    console.log(data.tracks.items[0].name);
+                    console.log(data.tracks.items[0].preview_url);
+                    console.log(data.tracks.items[0].album.name);
+                    console.log("----------");
+
+                });
+            } else if (dataList[0] === "movie-this") {
+                let movieURL = "http://www.omdbapi.com/?t=" + dataList[1] + "&y=&plot=short&apikey=trilogy";
+                // console.log(movieURL);
+                request(movieURL, function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        // console.log(JSON.parse(body));
+                        console.log("----------");
+                        console.log(`Title: ${JSON.parse(body).Title}`);
+                        console.log(`Year: ${JSON.parse(body).Year}`);
+                        console.log(`IMDB Rating: ${JSON.parse(body).imdbRating}`);
+                        console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`);
+                        console.log(`Country: ${JSON.parse(body).Country}`);
+                        console.log(`Language: ${JSON.parse(body).Language}`);
+                        console.log(`Plot: ${JSON.parse(body).Plot}`);
+                        console.log(`Actors: ${JSON.parse(body).Actors}`);
+                        console.log("----------");
+                    }
+                })
+            };
+        };
+    });
+
+};
